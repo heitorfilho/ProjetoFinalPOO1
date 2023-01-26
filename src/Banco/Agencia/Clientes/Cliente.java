60705e5438184e585a9883de8289fb6405e05853
Heitor Fillho <heitor.filho@ufu.br>
2023-01-26 17:36:09 -0300

    Cliente.java

--- src/Banco/Agencia/Clientes/Cliente.java
+++ src/Banco/Agencia/Clientes/Cliente.java
@@ -1,5 +1,24 @@
 package Banco.Agencia.Clientes;
 
-public class Cliente {
+import Util.Data;
+import Util.Pessoa;
+import Util.Endereco;
+
+public class Cliente extends Pessoa{
+
+    private String escolaridade;
+
+    public Cliente(String nome, String cpf, String sexo, Data dataNascimento, Endereco endereco, String estadoCivil, String escolaridade) {
+        super(nome, cpf, sexo, dataNascimento, endereco, estadoCivil);
+        this.escolaridade = escolaridade;
+    }
+
+    public String getEscolaridade() {
+        return this.escolaridade;
+    }
+
+    public void setEscolaridade(String escolaridade) {
+        this.escolaridade = escolaridade;
+    }
     
 }
