a71c900d7b7c4f28730d7f8c7a6a9e672c5c7236
Heitor Fillho <heitor.filho@ufu.br>
2023-01-28 17:01:04 -0300

    Update Gerente, Funcionario e Agencia

--- src/Banco/Agencia/Funcionarios/Funcionario.java
+++ src/Banco/Agencia/Funcionarios/Funcionario.java
@@ -1,5 +1,33 @@
 package Banco.Agencia.Funcionarios;
 
-public class Funcionario {
+
+import Util.Data;
+import Util.Endereco;
+import Util.Pessoa;
+
+public class Funcionario extends Pessoa{
+
+    private String nroCarteiraTrabalho;
+    private String rgFuncionario;
+    private String cargo;
+    private float salario;
+    private int anoIngresso;
+
+
+    public Funcionario(String nome, String cpf, String sexo, Data dataNascimento, Endereco endereco, String estadoCivil,
+        String nroCarteiraTrabalho, String rgFuncionario, String cargo, float salario, int anoIngresso) {
+        super(nome, cpf, sexo, dataNascimento, endereco, estadoCivil);
+        this.nroCarteiraTrabalho = nroCarteiraTrabalho;
+        this.rgFuncionario = rgFuncionario;
+        this.cargo = cargo;
+        this.salario = salario;
+        this.anoIngresso = anoIngresso;
+    }
+
+    public Funcionario() {
+        super(null, null, null, null, null, null);
+    }
+
+    
     
 }
--- src/Banco/Agencia/Funcionarios/Gerente.java
+++ src/Banco/Agencia/Funcionarios/Gerente.java
@@ -6,17 +6,25 @@ import Util.Endereco;
 
 public class Gerente extends Funcionario{
 
+    private Data dataIngresso;
     private Agencia agenciaGerenciada;
+    private boolean formacaoBasica;
 
 
-    public Gerente(String nome, String cpf, String sexo, Data dataNascimento, Endereco endereco, String estadoCivil,
-            String escolaridade, Agencia agenciaGerenciada) {
-        //super(nome, cpf, sexo, dataNascimento, endereco, estadoCivil, escolaridade);
+    public Gerente(String nome, String cpf, String sexo, Data dataNascimento, Endereco endereco, String estadoCivil, String nroCarteiraTrabalho,
+        String rgFuncionario, float salario, int anoIngresso, Data dataIngresso, Agencia agenciaGerenciada) {
+        super(nome, cpf, sexo, dataNascimento, endereco, estadoCivil, nroCarteiraTrabalho, rgFuncionario, "Gerente", salario, anoIngresso);
+        this.dataIngresso = dataIngresso;
         this.agenciaGerenciada = agenciaGerenciada;
+        this.formacaoBasica = true;
     }
 
     public Gerente() { // Construtor default
-        //super(null, null, null, null, null, null, null);
+        super(null, null, null, null, null, null, null, null, null, 0, 0);
+    }
+
+    public Agencia getAgenciaGerenciada() {
+        return this.agenciaGerenciada;
     }
 
     public void setAgecencia(Agencia agencia){
