88c1b05c56b59f61e244ed3eb92dff0bbc0a4b4d
IgorAugust0 <augustoigor269@gmail.com>
2023-01-28 20:59:51 -0300

    update funcionarios gerente

--- src/Banco/Agencia/Funcionarios/Funcionario.java
+++ src/Banco/Agencia/Funcionarios/Funcionario.java
@@ -5,20 +5,21 @@ import Util.Data;
 import Util.Endereco;
 import Util.Pessoa;
 
-public class Funcionario extends Pessoa{
+public class Funcionario extends Pessoa {
 
     private String nroCarteiraTrabalho;
-    private String rgFuncionario;
+    private int rg_num; // numeros do RG
+    private String rg_uf; // UF do RG
     private String cargo;
     private float salario;
     private int anoIngresso;
 
-
     public Funcionario(String nome, String cpf, String sexo, Data dataNascimento, Endereco endereco, String estadoCivil,
-        String nroCarteiraTrabalho, String rgFuncionario, String cargo, float salario, int anoIngresso) {
+            String nroCarteiraTrabalho, int rg_num, String rg_uf, String cargo, float salario, int anoIngresso) {
         super(nome, cpf, sexo, dataNascimento, endereco, estadoCivil);
         this.nroCarteiraTrabalho = nroCarteiraTrabalho;
-        this.rgFuncionario = rgFuncionario;
+        this.rg_num = rg_num;
+        this.rg_uf = rg_uf;
         this.cargo = cargo;
         this.salario = salario;
         this.anoIngresso = anoIngresso;
@@ -28,6 +29,66 @@ public class Funcionario extends Pessoa{
         super(null, null, null, null, null, null);
     }
 
-    
-    
+    public String getNroCarteiraTrabalho() {
+        return this.nroCarteiraTrabalho;
+    }
+
+    public void setNroCarteiraTrabalho(String nroCarteiraTrabalho) {
+        this.nroCarteiraTrabalho = nroCarteiraTrabalho;
+    }
+
+    public int getRg_num() {
+        return this.rg_num;
+    }
+
+    public void setRg_num(int rg_num) {
+        this.rg_num = rg_num;
+    }
+
+    public String getRg_uf() {
+        return this.rg_uf;
+    }
+
+    public void setRg_uf(String rg_uf) {
+        this.rg_uf = rg_uf;
+    }
+
+    public String getCargo() {
+        return this.cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public float getSalario() {
+        return this.salario;
+    }
+
+    public void setSalario(float salario) {
+        this.salario = salario;
+    }
+
+    public int getAnoIngresso() {
+        return this.anoIngresso;
+    }
+
+    public void setAnoIngresso(int anoIngresso) {
+        this.anoIngresso = anoIngresso;
+    }
+
+    public String printFuncionario() {
+        String data = printPessoa() + ";" + this.nroCarteiraTrabalho + ";" + this.rg_num + ";" + this.rg_uf + ";"
+                + this.cargo + ";" + this.salario + ";" + this.anoIngresso;
+
+        return data;
+    }
+
+    public void printTodosFunc(){
+        System.out.println(this.getNome() + " - " + this.getCpf() +  " - " + this.getCargo());
+    }
+
+    public String printGerente(){
+        throw new IllegalAccessError("Acesso negado!");
+    }
 }
--- src/Banco/Agencia/Funcionarios/Gerente.java
+++ src/Banco/Agencia/Funcionarios/Gerente.java
@@ -4,31 +4,74 @@ import Banco.Agencia.Agencia;
 import Util.Data;
 import Util.Endereco;
 
-public class Gerente extends Funcionario{
+public class Gerente extends Funcionario {
 
-    private Data dataIngresso;
+    private Data dataIngressoGerente;
     private Agencia agenciaGerenciada;
     private boolean formacaoBasica;
+    private boolean estaEmUmaAgencia;
 
-
-    public Gerente(String nome, String cpf, String sexo, Data dataNascimento, Endereco endereco, String estadoCivil, String nroCarteiraTrabalho,
-        String rgFuncionario, float salario, int anoIngresso, Data dataIngresso, Agencia agenciaGerenciada) {
-        super(nome, cpf, sexo, dataNascimento, endereco, estadoCivil, nroCarteiraTrabalho, rgFuncionario, "Gerente", salario, anoIngresso);
-        this.dataIngresso = dataIngresso;
+    public Gerente(String nome, String cpf, String sexo, Data dataNascimento, Endereco endereco, String estadoCivil,
+            String nroCarteiraTrabalho, int rg_num,
+            String rg_uf, float salario, int anoIngresso, Data dataIngressoGerente, Agencia agenciaGerenciada,
+            boolean formacaoBasica, boolean estaEmUmaAgencia) {
+        super(nome, cpf, sexo, dataNascimento, endereco, estadoCivil, nroCarteiraTrabalho, rg_num, rg_uf, "Gerente",
+                salario, anoIngresso);
+        this.dataIngressoGerente = dataIngressoGerente;
         this.agenciaGerenciada = agenciaGerenciada;
-        this.formacaoBasica = true;
+        this.formacaoBasica = formacaoBasica;
+        // this.formacaoBasica = true;
+        if (agenciaGerenciada == null) {
+            this.estaEmUmaAgencia = false;
+        } else {
+            this.estaEmUmaAgencia = true;
+        }
     }
 
     public Gerente() { // Construtor default
-        super(null, null, null, null, null, null, null, null, null, 0, 0);
+        super(null, null, null, null, null, null, null, 0, null, null, 0, 0);
     }
 
     public Agencia getAgenciaGerenciada() {
-        return this.agenciaGerenciada; 
+        return this.agenciaGerenciada;
     }
 
-    public void setAgencia(Agencia agencia){
+    public void setAgencia(Agencia agencia) {
         this.agenciaGerenciada = agencia;
+        // this.estaEmUmaAgencia = true;
     }
-    
+
+    public Data getdataIngressoGerente() {
+        return this.dataIngressoGerente;
+    }
+
+    public void setdataIngressoGerente(Data dataIngressoGerente) {
+        this.dataIngressoGerente = dataIngressoGerente;
+    }
+
+    public boolean getFormacaoBasica() {
+        return this.formacaoBasica;
+    }
+
+    public void setFormacaoBasica(boolean formacaoBasica) {
+        this.formacaoBasica = formacaoBasica;
+    }
+
+    public boolean getEstaEmUmaAgencia() {
+        return this.estaEmUmaAgencia;
+    }
+
+    public void setEstaEmUmaAgencia(boolean estaEmUmaAgencia) {
+        this.estaEmUmaAgencia = estaEmUmaAgencia;
+    }
+
+    @Override
+    public String printFuncionario() {
+        String data = super.printFuncionario() + ";" + this.dataIngressoGerente.printData() + ";" + // this.agenciaGerenciada.print();
+               "Formacao basica: " + this.formacaoBasica + ";" + "Gerente esta em uma agencia: " + this.estaEmUmaAgencia + ";";
+
+        return data;
+
+    }
+
 }
