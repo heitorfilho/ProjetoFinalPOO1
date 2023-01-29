50a6d056860e384ec7f7f2da6768cff47a3bf445
IgorAugust0 <augustoigor269@gmail.com>
2023-01-29 18:33:32 -0300

    reorganiza

--- src/Util/Arquivos.java
+++ src/Util/Arquivos.java
@@ -27,43 +27,46 @@ public class Arquivos {
     ////////////////////////////////
 
     public static void salvarArquivoCliente(LinkedList<Cliente> clientes) {
-        
+
         try {
             FileWriter arq = new FileWriter(BaseDeDados + "Clientes.csv");
             PrintWriter out = new PrintWriter(arq);
             try {
-                for(Cliente cliente : clientes){
+                for (Cliente cliente : clientes) {
                     String linha = cliente.printCliente();
                     out.println(linha);
-                } 
-            } catch(NullPointerException erro) {
+                }
+            } catch (NullPointerException erro) {
                 System.out.println("Não possui nenhum registro" + erro);
-            } 
-            out.close(); 
+            }
+            out.close();
             arq.close();
-        } catch(IOException erro) {
+        } catch (IOException erro) {
             System.out.println("Erro na escrita dos dados dos clientes" + erro);
         }
-        
+
     }
 
-    public static LinkedList<Cliente> carregarClientes(){
+    public static LinkedList<Cliente> carregarClientes() {
         LinkedList<Cliente> clientes = new LinkedList<>();
 
         try {
             FileReader ent = new FileReader(BaseDeDados + "Clientes.csv");
             BufferedReader br = new BufferedReader(ent);
             String linha;
-            String []campos = null;
-            while((linha = br.readLine()) != null){
+            String[] campos = null;
+            while ((linha = br.readLine()) != null) {
                 campos = linha.split(";");
-                Data dataNascimento = new Data(Integer.parseInt(campos[2]), Integer.parseInt(campos[3]), Integer.parseInt(campos[4]));
-                Endereco endCliente = new Endereco(campos[5], campos[6], campos[7], campos[8], Integer.parseInt(campos[9]), linha, Integer.parseInt(campos[10]), campos[11]);
-                clientes.add(new Cliente(campos[0], campos[1], dataNascimento, endCliente, campos[13], campos[14], campos[15]));
-            } 
+                Data dataNascimento = new Data(Integer.parseInt(campos[2]), Integer.parseInt(campos[3]),
+                        Integer.parseInt(campos[4]));
+                Endereco endCliente = new Endereco(campos[5], campos[6], campos[7], campos[8],
+                        Integer.parseInt(campos[9]), linha, Integer.parseInt(campos[10]), campos[11]);
+                clientes.add(new Cliente(campos[0], campos[1], dataNascimento, endCliente, campos[13], campos[14],
+                        campos[15]));
+            }
             br.close();
             ent.close();
-        } catch(IOException erro){
+        } catch (IOException erro) {
             System.out.println("Erro na leitura dos dados dos clientes" + erro);
         }
         return clientes;
@@ -72,8 +75,8 @@ public class Arquivos {
     //////////////////////////////////
     //// ARQUIVO PARA FUNCIONARIOS //
     ////////////////////////////////
-    
-    public static LinkedList<Funcionario> carregarFuncionarios(int numAgencia){
+
+    public static LinkedList<Funcionario> carregarFuncionarios(int numAgencia) {
         LinkedList<Pessoa> funcionarios = new LinkedList<>();
         String numeroAgencia = String.valueOf(numAgencia);
 
@@ -81,47 +84,53 @@ public class Arquivos {
             FileReader ent = new FileReader(BaseDeDados + "\\Funcionarios\\" + numeroAgencia + "Funcionarios.csv");
             BufferedReader br = new BufferedReader(ent);
             String linha;
-            String []campos = null;
-            while((linha = br.readLine()) != null){
+            String[] campos = null;
+            while ((linha = br.readLine()) != null) {
                 campos = linha.split(";");
                 Data[] Datas = new Data[3];
-                Datas[0] = new Data(Integer.parseInt(campos[2]), Integer.parseInt(campos[3]), Integer.parseInt(campos[4]));
-                Datas[1] = new Data(Integer.parseInt(campos[2]), Integer.parseInt(campos[3]), Integer.parseInt(campos[4]));
-                
-                Endereco endFunc = new Endereco(campos[5], campos[6], campos[7], campos[8], Integer.parseInt(campos[9]), linha, Integer.parseInt(campos[10]), campos[11]);
-                if(campos[16].equals("Gerente")){
-                    Datas[2]= new Data
+                Datas[0] = new Data(Integer.parseInt(campos[2]), Integer.parseInt(campos[3]),
+                        Integer.parseInt(campos[4]));
+                Datas[1] = new Data(Integer.parseInt(campos[18]), Integer.parseInt(campos[19]),
+                        Integer.parseInt(campos[20]));
+                Endereco End = new Endereco(campos[5], Integer.parseInt(campos[6]), campos[7], campos[8], campos[9], campos[10], campos[11], Integer.parseInt(campos[12]));
+                if (campos[16].equals("Gerente")) {
+                    Datas[2] = new Data(Integer.parseInt(campos[26]), Integer.parseInt(campos[27]), Integer.parseInt(campos[28]));
+
+                    Funcionario novo = new Gerente(campos[0], campos[1], Datas[0], End, campos[13], campos[14], Integer.parseInt(campos[15]), Float.parseFloat(campos[17]), Datas[1], Integer.parseInt(campos[21]), campos[22], Boolean.parseBoolean(campos[23]), Datas[2]);
+                            funcionarios.add(novo);
+                } else {
+                    Funcionario novo = new Funcionario(campos[0], campos[1], Datas[0], End, campos[13], campos[14],
+                            Integer.parseInt(campos[15]), campos[16], Float.parseFloat(campos[17]), Datas[1],
+                            Integer.parseInt(campos[21]), campos[22]);
+                            funcionarios.add(novo);
                 }
-                funcionarios.add(new Funcionario(campos[0], campos[1], dataNascimento, endCliente, campos[13], campos[14], campos[15], Integer.parseInt(campos[16]), campos[17], campos[18], Float.parseFloat(campos[19], Integer.parseInt(campos[20]) ));
-            } 
+            }
             br.close();
-            ent.close();
-        } catch(IOException erro){
-            System.out.println("Erro na leitura dos dados dos clientes" + erro);
+        } catch (IOException erro) {
+            System.out.println(" Arquivo nao encontrado ou corrompido: Funcionarios.csv");
         }
         return funcionarios;
     }
 
     public static void salvarArquivoFuncionario(LinkedList<Funcionario> funcionarios) {
-        
+
         try {
             FileWriter arq = new FileWriter(BaseDeDados + "Funcionarios.csv");
             PrintWriter out = new PrintWriter(arq);
             try {
-                for(Funcionario funcionario : funcionarios){
+                for (Funcionario funcionario : funcionarios) {
                     String linha = funcionario.printFuncionario();
                     out.println(linha);
-                } 
-            } catch(NullPointerException erro) {
+                }
+            } catch (NullPointerException erro) {
                 System.out.println("Não possui nenhum registro" + erro);
-            } 
-            out.close(); 
+            }
+            out.close();
             arq.close();
-        } catch(IOException erro) {
+        } catch (IOException erro) {
             System.out.println("Erro na escrita dos dados dos funcionarios" + erro);
         }
-        
+
     }
 
-    
 }
--- src/Util/Endereco.java
+++ src/Util/Endereco.java
@@ -3,17 +3,16 @@ package Util;
 import java.util.Scanner;
 
 public class Endereco {
-    private String pais;
-    private String estado;
-    private String cidade;
-    private String bairro;
-    private int cep;
     private String rua;
     private int numero;
+    private String bairro;
+    private String cidade;
+    private String estado;
+    private String pais;
     private String complemento;
+    private int cep;
 
-    public Endereco(String pais, String estado, String cidade, String bairro, int cep, String rua, int numero,
-            String complemento) {
+    public Endereco(String rua, int numero, String bairro, String cidade, String estado, String pais, String complemento, int cep) {
         this.pais = pais;
         this.estado = estado;
         this.cidade = cidade;
@@ -24,69 +23,70 @@ public class Endereco {
         this.complemento = complemento;
     }
 
-    public String getPais() {
-        return this.pais;
-    }
-
-    public void setPais(String pais) {
-        this.pais = pais;
-    }
-
-    public String getEstado() {
-        return this.estado;
-    }
-
-    public void setEstado(String estado) {
-        this.estado = estado;
-    }
-
-    public String getCidade() {
-        return this.cidade;
-    }
-
-    public void setCidade(String cidade) {
-        this.cidade = cidade;
-    }
-
-    public String getBairro() {
-        return this.bairro;
-    }
-
-    public void setBairro(String bairro) {
-        this.bairro = bairro;
-    }
-
-    public int getCep() {
-        return this.cep;
-    }
-
-    public void setCep(int cep) {
-        this.cep = cep;
-    }
-
-    public String getRua() {
+    public String getRua(){
         return this.rua;
     }
 
-    public void setRua(String rua) {
-        this.rua = rua;
-    }
-
-    public int getNumero() {
+    public int getNumero(){
         return this.numero;
     }
 
-    public void setNumero(int numero) {
-        this.numero = numero;
+    public String getBairro(){
+        return this.bairro;
     }
 
-    public String getComplemento() {
+    public String getCidade(){
+        return this.cidade;
+    }
+
+    public String getEstado(){
+        return this.estado;
+    }
+
+    public String getPais(){
+        return this.pais;
+    }
+
+    public String getComplemento(){
         return this.complemento;
     }
 
-    public void setComplemento(String complemento) {
+    public int getCep(){
+        return this.cep;
+    }
+
+    public void setRua(String rua){
+        this.rua = rua;
+    }
+
+    public void setNumero(int numero){
+        this.numero = numero;
+    }
+
+    public void setBairro(String bairro){
+        this.bairro = bairro;
+    }
+
+    public void setCidade(String cidade){
+        this.cidade = cidade;
+    }
+
+    public void setEstado(String estado){
+        this.estado = estado;
+    }
+
+    public void setPais(String pais){
+        this.pais = pais;
+    }
+
+    public void setComplemento(String complemento){
         this.complemento = complemento;
     }
+
+    public void setCep(int cep){
+        this.cep = cep;
+    }
+
 
     public String printEndereco() {
         String Data = rua + "; " + numero + "; " + complemento + "; "
